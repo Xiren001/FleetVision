c869ca00cd0ff2977422de265df49f60dd8a5ff8
Myko Xiren Dungca <mykoxiren992003@gmail.com>
2025-01-31 02:05:53 +0800

    Add project files.

---
+++ FleetVision/Services/ApplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FleetVision.Services
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options) 
+        { 
+        }
+        //public DbSet<Info> infos { get; set; }
+    } 
+} 
